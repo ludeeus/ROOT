b98e174eda29f607bbf73dd3de592381fe230364
Ludeeus <ludeeus@ludeeus.dev>
2021-01-05 18:23:58 +0100

    test

--- waitforjob/index.js
+++ waitforjob/index.js
@@ -1,29 +1,9 @@
 const github = require('@actions/github');
 const core = require('@actions/core');
 
-function timeout(ms) {
+function asyncTimeout(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
-
-const target = "job1";
-let conclusion;
-let status;
-while (status !== "completed") {
-  await timeout(5000)
-  const res = await github.request('GET /repos/{owner}/{repo}/commits/{ref}/check-runs', {
-    owner: 'ludeeus',
-    repo: 'ROOT',
-    ref: '${{github.sha}}'
-  });
-  conclusion = res.data.check_runs.find((res) => res.name = target).conclusion;
-  status = res.data.check_runs.find((res) => res.name = target).status;  
-}
-
-console.log(status, conclusion);
-if (conclusion !== "success") {
-  core.setFailed(conclusion);
-}
-
 
 async function run() {
     const token = core.getInput('github-token');
@@ -38,7 +18,7 @@ async function run() {
     let tries = 0
 
     while (status !== "completed" || tries > retry_limit) {
-        await timeout(5000)
+        await asyncTimeout(timeout)
         const result = await octokit.request('GET /repos/{owner}/{repo}/commits/{ref}/check-runs', {
           owner: 'ludeeus',
           repo: 'ROOT',
